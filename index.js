c52216e6174b6f5ebea09f3aede94481ffa6fe31
sunam RImal Nepali <tsunamr@gmail.com>
2020-11-25 21:22:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function happyHolidays(christmas){
 function happyCustomHolidayTo(holiday, name){
   return(`Happy ${holiday}, ${name}!`)
 }
+function holidayCountdown(holiday, days){
+  return (`It's ${days} days until ${holiday}!`)
+
+}
